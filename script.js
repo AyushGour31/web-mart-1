0768a4038c9ff9210f15d05b6462b2d1ad15064a
Ayush Gour <ayushgour3110@gmail.com>
2024-04-21 20:36:55 +0530

    1.7

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 import { games } from "./games.js";
 import { applications } from "./applications.js";
+import { sliderItem } from "./slider.js";
 
 const game = document.querySelector(".games");
 const gameSec = document.querySelector(".games-sec");
 const apps = document.querySelector(".apps");
 // const btn = document.querySelector(".play-btn")
+
+setTimeout(()=>{
+    document.querySelector(".welcome").innerHTML = "";
+}, 3000)
 
 games.forEach((ele, i) => {
     const node = document.createElement("div")
@@ -44,16 +49,16 @@ applications.forEach((ele, i) => {
 
 function slider(i) {
   setTimeout(() => {
-    if (i == games.length) i = 0;
+    if (i == sliderItem.length) i = 0;
     
     game.innerHTML = `
         <div class="item item-${i}">
-            <img class="item-img" src="${games[i].favicon}" alt="">
+            <img class="item-img" src="${sliderItem[i].favicon}" alt="">
             <div class="item-delails">
-                <h2 class="name">${games[i].name}</h2>
-                <div class="tagline">${games[i].tagLine}</div>
-                <div class="rating">${games[i].rating}</div>
-                <button class="play-btn" type="button"><a href="${games[i].url}">Play</a></button>
+                <h2 class="name">${sliderItem[i].name}</h2>
+                <div class="tagline">${sliderItem[i].tagLine}</div>
+                <div class="rating">${sliderItem[i].rating}</div>
+                <button class="play-btn" type="button"><a href="${sliderItem[i].url}">Play</a></button>
             </div>
         </div>
     `;
---
+++ slider.js
@@ -0,0 +1,40 @@
+
+const sliderItem = [
+    {
+        "favicon" : "games/tic-tac-toe-2/favicon.png",
+        "name" : "Tic Tac Toe",
+        "tagLine" : "best game forever",
+        "rating" : "4.0 ⭐️⭐️⭐️⭐︎✩",
+        "url" : "games/tic-tac-toe-2/index.html"
+    },
+    {
+        "favicon" : "applications/clock/images/clock.png",
+        "name" : "Clock",
+        "tagLine" : "best app forever",
+        "rating" : "4.0 ⭐️⭐️⭐️⭐︎✩",
+        "url" : "applications/clock/index.html"
+    },
+    {
+        "favicon" : "games/colour-picker/icons8-color-wheel-96.png",
+        "name" : "Color Picker",
+        "tagLine" : "best game forever",
+        "rating" : "3.0 ⭐️⭐️⭐️✩✩",
+        "url" : "games/colour-picker/index.html"
+    },
+    {
+        "favicon" : "applications/currency-converter/android-chrome-192x192.png",
+        "name" : "Currency Converter",
+        "tagLine" : "best app forever",
+        "rating" : "3.0 ⭐️⭐️⭐️✩✩",
+        "url" : "applications/currency-converter/index.html"
+    },
+    {
+        "favicon" : "games/rock-paper-scissor/rock-paper-scissors.png",
+        "name" : "Rock Paper Scissor",
+        "tagLine" : "best game forever",
+        "rating" : "3.0 ⭐️⭐️⭐️✩✩",
+        "url" : "games/rock-paper-scissor/index.html"
+    },
+]
+
+export { sliderItem };
